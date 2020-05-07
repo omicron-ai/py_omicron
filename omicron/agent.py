bca8485b0a647c231c62de26e6cf82a95c54daf6
ielm <leoni@rpi.edu>
2020-05-07 10:43:02 -0400

    (wip) memory utilities and formatting

--- omicron/agent.py
+++ omicron/agent.py
@@ -1,49 +1,156 @@
+"""Create dialog agents for training and production.
+
+See module train for a mechanism for training agents.
+See module build for a mechanism for building agents for production.
+
+Classes:
+    Agent
+    Sentinel
+    Memory
+    Agenda
+    Context
+    Representation
+    Turn
+    Topic
+    AtomicCounter
+    Origin
+
+"""
+
 from typing import Union
 from collections import OrderedDict
 import networkx as nx
 import threading
 import uuid
 from omicron import stanza_nlp, OMICRON_NAMESPACE
-from omicron.utils import INDENT
-from omicron.nlp import tokens
+from omicron.utils import INDENT, IMAGE_DIR
+from omicron.nlp import get_tokens, get_topics
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
 
 
 class Agent:
-    def __init__(self, _agent: int):
-        self.xid = _agent
-        self.id = uuid.uuid5(OMICRON_NAMESPACE, f"{_agent}")
+    """The core omicron agent class.
+
+    Attributes:
+        xid
+        id
+        memory
+        sentinel
+        agenda
+        signals
+        signal_count
+    """
+
+    def __init__(self, _agent_index: int):
+        """
+        Initialize empty agent from an agent index. There can only be one agent per index per world.
+
+        :param _agent_index: integer describing agent index in world.
+        """
+        self.xid = _agent_index
+        self.id = uuid.uuid5(OMICRON_NAMESPACE, f"{_agent_index}")
         self.memory = Memory((self.xid, self.id))
         self.sentinel = self.memory.sentinel
         self.agenda = Agenda()
-        # _heartbeat_tempo = 0.25
-
         self.signals = []
         self.signal_count = AtomicCounter()
 
     def handle(self, _signal = None):
+        """
+        Handle agent signal. Signals are either input or output representations. If
+        the signal is None, handle the next signal on agenda.
+
+        :param _signal: representation of utterance to input or output
+        """
         if _signal:
             self.agenda.add_signal(_signal)
 
         sid, signal = self.agenda.get_next_signal()
 
-        print(signal)
-        if signal["mode"] == "output":
-            self.output(signal["turn"])
-        if signal["mode"] == "input":
-            self.input(signal["turn"])
+        if signal['mode'] == 'output':
+            self.output(signal['turn'])
+        if signal['mode'] == 'input':
+            self.input(signal['turn'])
 
-    def input(self, _signal):
-        # turn['tokens'] = tokens(turn['text'])
-        # take in as input a dialog turn from another agent.
-        # process turn, determine dialog act/ask, etc.
-        # update memory with input turn
-        self.memory.add_input(_signal)
-        print(f"{INDENT}AGENT {self.xid}\t<INPUT>:\t{_signal['text']}")
+    def input(self, _signal, _verbose: bool = False):
+        """
+        Input utterance from dialog partner.
 
-    def output(self, _signal: dict):
-        # generate and output a dialog turn from a semantic representation
-        # update memory with output turn
-        print(f"{INDENT}AGENT {self.xid}\t<OUTPUT>:\t{_signal['intent']}({_signal['semantic_slot']}) -> {_signal['text']}")
+        :param _signal: signal representation to input
+        :param _verbose: write process log to console
+        """
+        input_turn = Turn(self.xid, {'turn': _signal['turn'], 'text': _signal['text']})
+        input_tokens = get_tokens(input_turn.text)
+        input_topics = get_topics(input_tokens)
+        input_rep = Representation(self.xid, {'turn': _signal['turn'],
+                                              'intent': _signal['__intent__'],
+                                              'sem_slot': _signal['__semantic_slot__'],
+                                              'agent': _signal['agent'],
+                                              'directed_to': input_turn.directed_to,
+                                              'tokens': input_tokens,
+                                              'topics': input_topics})
+
+        self.memory.add_input(input_turn, input_rep)
+
+        if _verbose:
+            print(f"{INDENT}AGENT {self.xid}\t<INPUT>:\t{_signal['text']}")
+
+    def output(self, _signal: dict, _verbose: bool = False):
+        """
+        Output utterance to dialog partner.
+
+        :param _signal: signal representation to output
+        :param _verbose: write process log to console
+        """
+        output_turn = Turn(self.xid, {'turn': _signal['turn'], 'text': _signal['text']})
+        output_tokens = get_tokens(output_turn.text)
+        output_topics = get_topics(output_tokens)
+        output_rep = Representation(self.xid, {'turn': _signal['turn'],
+                                               'intent': _signal['intent'],
+                                               'sem_slot': _signal['semantic_slot'],
+                                               'agent': _signal['agent'],
+                                               'directed_to': output_turn.directed_to,
+                                               'tokens': output_tokens,
+                                               'topics': output_topics})
+
+        self.memory.add_output(output_turn, output_rep)
+
+        if _verbose:
+            print(f"{INDENT}AGENT {self.xid}\t<OUTPUT>:\t{_signal['intent']}({_signal['semantic_slot']}) -> {_signal['text']}")
+
+    def render_memory(self, filename: str = None):
+        """Render memory as a DOT graph. (WIP)
+        """
+
+        # val_map = {'A': 1.0,
+        #            'D': 0.5714285714285714,
+        #            'H': 0.0}
+
+        # val_map = {'A': 1.0,
+        #            'T': 0.6,
+        #            'R': 0.6,
+        #            'O': 0.0}
+        #
+        # values = [val_map.get(node.type, 0.45) for node in self.memory.nodes()]
+        # edge_labels=dict([((u,v,),d['label'])
+        #                   for u,v,d in self.memory.edges(data=True)])
+        # red_edges = [('C','D'),('D','A')]
+        # edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
+
+        # pos=nx.spring_layout(self.memory)
+        # nx.draw_networkx_edge_labels(self.memory,pos,edge_labels=edge_labels)
+        # nx.draw(G,pos, node_color = values, node_size=1500,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
+        # nx.draw(self.memory,pos, node_color = values, node_size=1500,edge_cmap=plt.cm.Reds)
+        # pylab.show()
+        if filename is None:
+            filename = f"a{self.xid}_memory.png"
+        A = nx.nx_agraph.to_agraph(self.memory)
+        A.layout(prog="dot")
+        # print(A)
+        A.draw(f"{IMAGE_DIR}/{filename}")
+        # pos = nx.nx_agraph.graphviz_layout(self.memory, prog='dot')
+        # pass
 
     def __str__(self):
         return f"_agent_{self.xid}_"
@@ -53,17 +160,18 @@ class Agent:
 
 
 class Sentinel:
+    """Abstract agent in memory.
+
+    Attributes:
+        id
+        agent
+        type
+
+    """
     def __init__(self, seed):
-        self._id = uuid.uuid5(OMICRON_NAMESPACE, f"{seed[1]}")
-        self._agent = OrderedDict(zip(['SIMPLE', 'UUID'], seed))
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def agent(self):
-        return self._agent
+        self.id = uuid.uuid5(OMICRON_NAMESPACE, f"{seed[1]}")
+        self.agent = OrderedDict(zip(['SIMPLE', 'UUID'], seed))
+        self.type = "A"
 
     def __str__(self):
         return f"_agent_{self.agent['SIMPLE']}_"
@@ -74,23 +182,43 @@ class Sentinel:
 
 class Memory(nx.MultiDiGraph):
     def __init__(self, seed):
-        super(Memory, self).__init__()
+        super(Memory, self).__init__(rankdir="LR", mode="scale")
         self.sentinel = Sentinel(seed)
+        self.agents = {}
         self.context = Context()
         self.turn_count = AtomicCounter()
         self.origin = Origin()
         super().add_node(self.origin, label=f"{self.origin}")
         super().add_node(self.sentinel, label=f"SELF_{self.sentinel}")
-        super().add_edge(self.sentinel, self.origin, tag="IN")
+        super().add_edge(self.sentinel, self.origin, tag='IN')
 
-    def add_input(self, _signal):
-        self.add_node(_signal, tag=f"")
-        pass
+    def add_agent(self, agent_index):
+        agent_id = uuid.uuid5(OMICRON_NAMESPACE, f"{agent_index}")
+        sentinel = Sentinel((agent_index, agent_id))
+        self.agents[sentinel.agent['SIMPLE']] = sentinel
+        self.add_node(sentinel, label=f"AGENT_{sentinel}")
+        self.add_edge(sentinel, self.origin, tag='IN')
 
-    def add_output(self, signal):
-        pass
+    def add_input(self, turn, rep):
+        if rep.directed_to not in self.agents.keys():
+            self.add_agent(rep.directed_to)
 
+        self.add_node(turn)
+        self.add_node(rep)
+        self.add_edge(self.agents[rep.agent], turn, label="GENERATES")
+        self.add_edge(turn, self.sentinel, label="INPUT")
+        self.add_edge(self.sentinel, rep, label="PRODUCES")
+        self.add_edge(rep, turn, label="REPRESENTS")
 
+    def add_output(self, turn, rep):
+        if rep.directed_to not in self.agents.keys():
+            self.add_agent(rep.directed_to)
+        self.add_node(turn)
+        self.add_node(rep)
+        self.add_edge(self.sentinel, turn, label="GENERATES")
+        self.add_edge(turn, self.agents[rep.directed_to], label="INPUT")
+        self.add_edge(self.sentinel, rep, label="PRODUCES")
+        self.add_edge(rep, turn, label="REPRESENTS")
 
 
 class Agenda:
@@ -100,7 +228,7 @@ class Agenda:
         self.counter = AtomicCounter()
 
     def add_signal(self, signal):
-        sid = f"G-{self._counter.value}"
+        sid = f"G-{self.counter.value}"
         self.store[sid] = signal
         self.counter.increment()
         return sid
@@ -189,105 +317,49 @@ class Context:
 
 
 class Representation:
-    def __init__(self, ):
-        self.intent = ""
-        self.sem_slot = ""
-        self.tokens = {}
-        self.topic_tokens = []
+    def __init__(self, seed, rep):
+        self.id = uuid.uuid5(OMICRON_NAMESPACE, f"{seed}")
+        self.turn = rep['turn']
+        self.intent = rep['intent']
+        self.agent = rep['agent']
+        self.directed_to = rep['directed_to']
+        self.sem_slot = rep['sem_slot']
+        self.tokens = rep['tokens']
+        self.topic_tokens = rep['tokens']
+        self.type = "R"
 
-    # def __str__(self):
+
+    def __str__(self):
+        return f"_repr_{self.turn}_"
+
+    def __repr__(self):
+        return f"<_REPR_{self.id}>_"
+
 
 class Turn:
     def __init__(self, seed, turn):
-        self._index = turn['turn']
-        self._id = uuid.uuid5(OMICRON_NAMESPACE, f"{seed[1]}")
-        self._intent = ""
-        self._slot = ""
-        self._agent = OrderedDict(zip(['SIMPLE', 'UUID'], seed))
-        self._directed_to = f"1" if f"{self.agent['SIMPLE']}" == f"0" else f"0"
-        self._text = ""
-        self._terminals = ""
-
-    @property
-    def intent(self):
-        return self._intent
-
-    @intent.setter
-    def intent(self, value):
-        self._intent = value
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def index(self):
-        return self._index
-
-    @property
-    def agent(self):
-        return self._agent
-
-    @agent.setter
-    def agent(self, value):
-        self._agent = value
-
-    @property
-    def text(self):
-        return self._text
-
-    @text.setter
-    def text(self, value):
-        self._text = value
-
-    @property
-    def terminals(self):
-        return self._terminals
-
-    @terminals.setter
-    def terminals(self, value):
-        self._terminals = value
+        self.index = turn['turn']
+        self.id = uuid.uuid5(OMICRON_NAMESPACE, f"{seed}")
+        self.directed_to = f'1' if f"{seed}" == f'0' else f'0'
+        self.text = turn['text']
+        self.type = "T"
 
     def __str__(self):
-        return f"{self.id}"
+        return f"_turn_{self.index}_"
 
     def __repr__(self):
-        return f"{self.id}"
+        return f"<_TURN_{self.id}_>"
 
 
 class Topic:
 
     def __init__(self, constituents, user, index, cutoff):
-        self._id = f"TOPIC-{index}"
-        self._constituents = constituents
-        self._posted_by = user
-        self._topic_index = index
-        self._cutoff = cutoff
+        self.id = f"TOPIC-{index}"
+        self.constituents = constituents
+        self.posted_by = user
+        self.topic_index = index
+        self.cutoff = cutoff
         self._counter = AtomicCounter()
-
-    @property
-    def id(self):
-        return self._id
-
-    @id.setter
-    def id(self, value):
-        self._id = value
-
-    @property
-    def user(self):
-        return self._posted_by
-
-    @user.setter
-    def user(self, value):
-        self._posted_by = value
-
-    @property
-    def cutoff(self):
-        return self._cutoff
-
-    @cutoff.setter
-    def cutoff(self, value):
-        self._cutoff = value
 
     @property
     def count(self):
@@ -340,6 +412,9 @@ class AtomicCounter:
 
 
 class Origin:
+    def __init__(self):
+        self.type = "O"
+
     def __str__(self):
         return f"Origin "
 
