7d1d0f6da563593f668d43c37ad8d69027365d21
ielm <leoni@rpi.edu>
2020-05-20 17:06:02 -0400

    move images to resources

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="omicron",
     version="0.0.2",
+    author="Ivan Leon",
+    author_email="leoni@rpi.edu",
+    description="An agent framework for training semantically-driven conversational agents.",
+    url="https://github.com/omicron-ai/omicron",
     packages=find_packages(),
 
     install_requires=[
@@ -10,11 +14,14 @@ setup(
         "stanza"
     ],
 
-    author="Ivan Leon",
-    author_email="leoni@rpi.edu",
-    description="An agent framework for training semantically-driven conversational agents.",
     keywords="ai, agent",
     project_urls={
         "Source Code": "https://github.com/omicron-ai/omicron",
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
